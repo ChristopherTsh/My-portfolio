21553f60fc9662e7c4eaa512390ba60764eff555
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-13 20:18:48 +0200

    "Updated ConnectSection component with new social media links, CV download link, and revised layout and styling."

--- src/ConnectSection.jsx
+++ src/ConnectSection.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 
 const ConnectSection = () => {
+  const cvLink = 'https://docs.google.com/document/d/your-doc-id/export?format=docx';
+
   return (
-    <section id="connect" className="px-4 py-16 mx-auto max-w-7xl">
-      <div className="text-center mb-12">
-        <h2 className="text-4xl font-extrabold leading-tight text-gray-800">Connect with Me</h2>
+    <section id="connect" className="px-4 py-16 mx-auto max-w-7xl text-center">
+      <h2 className="text-3xl font-extrabold leading-tight text-dark mb-8">
+        Connect with Me
+      </h2>
+      <div className="flex justify-center space-x-4 mb-8">
+        <a href="https://github.com/ChristopherTsh" target="_blank" rel="noopener noreferrer">
+          <img src="../src/icons/github.png" alt="GitHub" className="h-8 w-8" />
+        </a>
+        <a href="https://www.linkedin.com/in/christophertshoma" target="_blank" rel="noopener noreferrer">
+          <img src="../src/icons/linkedin-logo-png-2025.png" alt="LinkedIn" className="h-8 w-8" />
+        </a>
+        <a href="https://twitter.com/ChristopherTshoma" target="_blank" rel="noopener noreferrer">
+          <img src="../src/icons/twitter-icon-98.png" alt="Twitter" className="h-8 w-8" />
+        </a>
+        <a href="https://www.youtube.com/@ChristopherTshoma" target="_blank" rel="noopener noreferrer">
+          <img src="../src/icons/youtube-logo-png-3572.png" alt="YouTube" className="h-8 w-8" />
+        </a>
+        <a href="https://medium.com/@ChristopherTshoma" target="_blank" rel="noopener noreferrer">
+          <img src="../src/icons/medium.png" alt="Medium" className="h-8 w-8" />
+        </a>
       </div>
-      <div className="flex justify-center space-x-6">
-        <a href="https://linkedin.com/in/christopher-tshoma" className="text-blue-500 hover:text-blue-700">
-          <i className="fab fa-linkedin fa-3x"></i>
+      <div>
+        <a
+          href="https://drive.usercontent.google.com/u/0/uc?id=1KCmREkTvO5A81zWzphpWKfSD8xMvsD0m&export=download"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+          download
+        >
+          Download My CV
         </a>
-        <a href="https://github.com/ChristopherTsh" className="text-gray-900 hover:text-gray-700">
-          <i className="fab fa-github fa-3x"></i>
-        </a>
-        <a href="https://twitter.com/christopher_tsh" className="text-blue-400 hover:text-blue-600">
-          <i className="fab fa-twitter fa-3x"></i>
-        </a>
-        {/* Add more social icons here */}
       </div>
     </section>
   );
